2c66987d92a296803888a6653b8840d305b2b933
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2025-12-02 15:31:41 +0530

    feat: enhance header component with user menu and loading state; add buttons for pricing and explore

--- components/header.tsx
+++ components/header.tsx
@@ -5,8 +5,15 @@ import { Authenticated, Unauthenticated } from "convex/react";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { BarLoader } from "react-spinners";
+import { useStoreUser } from "@/hooks/use-store-user";
+import { useState } from "react";
+import { BuildingIcon, PlusIcon, TicketIcon } from "lucide-react";
 
 export default function Header() {
+  const { isLoading } = useStoreUser();
+
+  const [showUpgradeModal, setShowUpgradeModal] = useState(false);
+
   return (
     <>
       <nav className="fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-xl z-20 border-b">
@@ -21,11 +28,44 @@ export default function Header() {
           {/* <div className="">Search and location</div> */}
 
           {/* Right Section */}
+          <div className="flex items-center gap-2">
+            <Button size={"sm"} variant={"ghost"} onClick={() => {}}>
+              Pricing
+            </Button>
+            <Button size={"sm"} variant={"ghost"} onClick={() => {}}>
+              <Link className="cursor-default" href={"explore"}>
+                Explore
+              </Link>
+            </Button>
 
-          <div className="flex items-center ">
             <Authenticated>
-              {/* Create Event */}
-              <UserButton />
+              <Button
+                size={"sm"}
+                asChild
+                variant={"outline"}
+                className="flex gap-1"
+              >
+                <Link href={"create-event"}>
+                  <PlusIcon className="size-4" />
+                  <span className="hidden sm:inline">Create Event</span>
+                </Link>
+              </Button>
+
+              <UserButton>
+                <UserButton.MenuItems>
+                  <UserButton.Link
+                    label="My Tickets"
+                    labelIcon={<TicketIcon size={16} />}
+                    href="/my-tickets"
+                  />
+                  <UserButton.Link
+                    label="My Events"
+                    labelIcon={<BuildingIcon size={16} />}
+                    href="/my-events"
+                  />
+                  <UserButton.Action label="manageAccount" />
+                </UserButton.MenuItems>
+              </UserButton>
             </Authenticated>
 
             <Unauthenticated>
@@ -38,9 +78,11 @@ export default function Header() {
         {/* MOBILE SEARCH AND LOCATION */}
 
         {/* LOADER */}
-        <div className="absolute bottom-0 left-0 w-full">
-          <BarLoader width={"100%"} color="#a855f7" />
-        </div>
+        {isLoading && (
+          <div className="absolute bottom-0 left-0 w-full">
+            <BarLoader width={"100%"} color="#a855f7" />
+          </div>
+        )}
       </nav>
 
       {/* MODALS */}
